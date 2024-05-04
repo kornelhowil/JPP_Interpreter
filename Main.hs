68099fd1db234ef4ec5d9f4dbe42c6ea825a0e98
kornelhowil <kornelhowil@gmail.com>
2024-05-04 18:52:04 +0200

    Final commit? v2

--- Main.hs
+++ Main.hs
@@ -8,8 +8,8 @@ import ParGrammar
 import Interpreter (interpret)
 import TypeChecker (typeCheck)
 
-run :: String -> IO ()
-run s = case pProgram $ myLexer s of
+runInterpreter :: String -> IO ()
+runInterpreter s = case pProgram $ myLexer s of
     Left p_err ->  hPutStrLn stderr $ "Parsing error: " ++ p_err
     Right parsed -> case typeCheck parsed of
         Left t_err -> hPutStrLn stderr $ "Static error: " ++ show t_err
@@ -19,5 +19,5 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [] -> getContents >>= run
-        fs -> mapM_ (\f -> putStrLn ("======== " ++ f ++ " =======") >> readFile f >>= run) fs
+        [] -> getContents >>= runInterpreter
+        file -> mapM_ (\f -> readFile f >>= runInterpreter) file
